1ab27933da46b2867947189150f79ff7bf948ae3
Vladimir Nikulin <mail.jorey@gmail.com>
2025-08-30 19:23:23 +0300

    Сверстана страница "Crew"

--- src/ts/crew.ts
+++ src/ts/crew.ts
@@ -1,11 +1,11 @@
 import { update_active_tab } from "./tab_switcher";
 
-const tab_buttons = document.getElementsByClassName("tab-panel-navigation__button");
+const tab_buttons = document.getElementsByClassName("tab-panel-pagination__button");
 
 let current_active = {
-    button: document.getElementById("mId__moon-tab-button") as HTMLElement,
-    picture: document.querySelector<HTMLElement>('.pictures__img[data-tab-name="moon"]'),
-    tab: document.querySelector<HTMLElement>('.tab-panel__tab[data-tab-name="moon"]'),
+    button: document.getElementById("mId__douglas-tab-button") as HTMLElement,
+    picture: document.querySelector<HTMLElement>('.pictures__img[data-tab-name="douglas"]'),
+    tab: document.querySelector<HTMLElement>('.tab-panel__tab[data-tab-name="douglas"]'),
 };
 
 // Навешиваем обработчики на все кнопки
